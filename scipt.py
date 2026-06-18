368b3b564518bf4bb26b636d0bc52f6b2dc7b43e
artemix1508 <artem4ik15082013@gmail.com>
2026-06-18 13:14:56 +0200

    removed the -placeholder for now" from the commandlist variable, as it is a functioning command now. Also added a new command "" to the commandlist variable, which is a placeholder for now.

--- scipt.py
+++ scipt.py
@@ -4,14 +4,14 @@ from win11toast import toast
 
 operatingsystem = [platform.system(), platform.release(), platform.version()]
 
-commandlist = ["help", "exit", "calculator - placeholder for now", "reminder - placeholder for now"]
+commandlist = ["help", "exit", "calculator", "reminder",]
 
 print(f"Operating System: {operatingsystem[0]} {operatingsystem[1]} {operatingsystem[2]}")
 
 print("Hello, Welcome to 'Automate The Boring Stuff' or 'ATBS' for short!")
 
 print("NOTE: enter help to get the full list of available commands.")
-#To do: add more FUNCTIONING commands cuz i only have 2 working commands right now, and the rest are just placeholders for now.
+
 while True:
     command = input("Enter a command: ")
     if command == "help":
@@ -44,8 +44,5 @@ while True:
             print(f"Success: Reminder set for {timetoremind} seconds from now!")
         except ValueError:
             print("Invalid input. Please enter a valid number for the time.")
-
-
-
     else:
         print(f"Unknown command: {command}. Type 'help' to see the list of available commands.")
